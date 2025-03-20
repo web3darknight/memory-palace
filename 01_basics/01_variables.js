88a910948ca1cc70459c4ea3df9a7434031866ac
web3darknight <164993445+web3darknight@users.noreply.github.com>
2025-03-20 11:35:31 +0000

    main

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553 
+let accountEmail = "@gmail.com" //let can have two values//
+let accountCity= "Jaipur"  // var can also have two values , but const cannot //
+let accountPassword ="12345"
+console.table([accountEmail, accountId, accountPassword, accountCity])
+
+// const accountId = 2 // n0t allowed
+ accountEmail = "hc@hc.com"
+ accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(accountId);
+
