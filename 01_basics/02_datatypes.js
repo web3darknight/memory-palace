6e798ab403fbf6d3b9fa84dffd7e0e3d7cf33933
web3darknight <164993445+web3darknight@users.noreply.github.com>
2025-03-21 06:05:33 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"// treat all JS code as newer version 
+
+// alert ( 3 + 3 ) we are using nodejs, not browser   
+
+console.log(3+3) //code readability should be high//
+console.log("mayur")
+
+let name = "Mayur"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => True/False
+// null => is a standalone value 
+// undefined 
+// symbol => unique
+
+// object 
+
+console.log(typeof "mayur")
+console.log(typeof null) // null is a type of object 
+console.log(typeof undefined) // undefined only
