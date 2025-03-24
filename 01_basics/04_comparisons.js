0d693205472f2caa68b37f9a6190258611f53abb
web3darknight <164993445+web3darknight@users.noreply.github.com>
2025-03-24 06:29:42 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,16 @@
+//      console.log(2 > 1);    
+//console.log(2 >= 1);    
+//console.log(2 < 1);    
+//console.log(2 == 1);    
+//console.log(2 != 1);      
+
+    //console.log("2">1);
+    //console.log("02" > 1)
+
+    console.log(null>0)
+    console.log(null==0)// ????????????????? why false since null is converted to 0 in JS //
+    console.log(null>=0) 
+
+    console.log(undefined == 0)  // comparisons and equality check work differently
+    console.log(undefined > 0)   
+    console.log(undefined < 0)
