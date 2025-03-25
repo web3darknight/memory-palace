084f1b399d341011f54b5701bca0bcf20e8d49de
web3darknight <164993445+web3darknight@users.noreply.github.com>
2025-03-25 13:18:49 +0000

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate = new Date()
+   // console.log(myDate.toString())
+    //console.log(myDate.toDateString())
+    //console.log(myDate.toLocaleString())  
+    //console.log(typeof myDate); // .... (object)
+
+    //let     myCreatedDate = new Date(2023, 0 ,23)
+    let myCreatedDate = new Date("2023-01-14")
+ // console.log(myCreatedDate.toDateString())// this gives values Mon Jan 23 2023//
+ console.log(myCreatedDate.toLocaleString()) // this gives values 1/23/2023, 5:03:00 AM//
